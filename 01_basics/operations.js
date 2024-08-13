a3ba9beb65975703a6c046180ed8550e3e9a31e4
githubrsh <social.rsh11@gmail.com>
2024-08-13 15:12:39 +0000

    Diiferent operations in js

---
+++ 01_basics/operations.js
@@ -0,0 +1,15 @@
+let value =3
+let NegValue = -value
+// console.log(NegValue);
+
+let str= "hEllo"
+let str2= " Rishabh"
+//console.log(str+str2)
+ 
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1"+ 2 + 1);
+// console.log(1+ 2 + "1");
+
+//console.log(+true);
+console.log(+"");
