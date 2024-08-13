0ed5587ba407913cfa1e50284ef843bbb86d1099
githubrsh <social.rsh11@gmail.com>
2024-08-13 14:57:57 +0000

    Data types Conversion

---
+++ 01_basics/DataTypeConversion.js
@@ -0,0 +1,37 @@
+// let score = '33aa'
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// let score = '33'
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let score = null
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// let score = undefined
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// notes "33"=>33
+//"33abs"=> NaN
+
+
+
